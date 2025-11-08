b27c43c19da9c1389eaed9dc8fdd54226e2d5964
DONHATMINH <donhatminhdofi@gmail.com>
2025-11-08 16:58:11 +0700

    diChuyen

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,6 +4,14 @@ public class Player : MonoBehaviour
 {
     private float moveMent;
     [SerializeField] private float moveSpeed = 5f;
+    private bool facingRight = true;
+    Rigidbody2D rb;
+    [SerializeField] private float jumpHight = 5f;
+    private bool isGround = true;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
     void Start()
     {
         
@@ -13,9 +21,28 @@ public class Player : MonoBehaviour
     void Update()
     {
         moveMent = Input.GetAxis("Horizontal");
+        if (moveMent < 0f && facingRight)
+        {
+            transform.eulerAngles = new Vector3(0f, -180f, 0f);
+            facingRight = false;
+        }
+        else if (moveMent > 0f && facingRight == false)
+        {
+            transform.eulerAngles = new Vector3(0f, 0f, 0f);
+            facingRight = true;
+        }
+        if (Input.GetKey(KeyCode.Space) && isGround == true) 
+        {
+            jump();
+            isGround = false;
+        }
     }
     private void FixedUpdate()
     {
         transform.position += new Vector3(moveMent, 0f, 0f) *moveSpeed* Time.deltaTime;
+    }
+    private void jump()
+    {
+        rb.AddForce(new Vector2 (0f, jumpHight), ForceMode2D.Impulse);   
     }
 }
